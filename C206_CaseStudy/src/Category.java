03809058ff4ee8b993aa1ba5d1613c4338528e35
fatmosthenotsogreat <83437047+fatmosthenotsogreat@users.noreply.github.com>
2021-08-01 23:24:49 +0800

    Category Class (Amos)

--- C206_CaseStudy/src/Category.java
+++ C206_CaseStudy/src/Category.java
@@ -1,4 +1,32 @@
 
 public class Category {
-
+	private String catName;
+	private int catId;
+	public Category(String catName, int catId) {
+		super();
+		this.catName = catName;
+		this.catId = catId;
+	}
+	
+	public String getCatName() {
+		return catName;
+	}
+	
+	public void setCatName(String catName) {
+		this.catName = catName;
+	}
+	
+	public int getCatId() {
+		return catId;
+	}
+	
+	public void setCatId(int catId) {
+		this.catId = catId;
+	}
+	
+	@Override
+	public String toString() {
+		return "CCACategory [name=" + catName + ", id=" + catId + "]";
+	}
+	
 }
