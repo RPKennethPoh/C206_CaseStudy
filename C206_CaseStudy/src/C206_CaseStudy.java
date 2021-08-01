bfbe6633acc4e56def9203bb4a1acd70ea1c8d01
fatmosthenotsogreat <83437047+fatmosthenotsogreat@users.noreply.github.com>
2021-08-02 00:52:05 +0800

    initiated the arraylist, changed the arraylist to a static object

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 
 public class C206_CaseStudy {
-	ArrayList<Category> categoryArrList;
+	private static ArrayList<Category> categoryArrList;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		categoryArrList = new ArrayList<Category>();
 
 	}
 	
