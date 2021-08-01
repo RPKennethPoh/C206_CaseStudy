d50dd45eb21132f1ceda27e7fd160183e4a928b9
fatmosthenotsogreat <83437047+fatmosthenotsogreat@users.noreply.github.com>
2021-08-01 23:42:43 +0800

    addCategory method imported arraylist created new arraylist of category
    the method returns a boolean. use the boolean to check if the new
    category object is added into the arraylist

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
+	ArrayList<Category> categoryArrList;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	//Add category
+	private boolean addCategory(int catId, String catName) {
+		boolean added = false;
+		
+		Category category = new Category(catId, catName);
+		
+		int oldArrListSize = categoryArrList.size();
+		categoryArrList.add(category);
+		
+		int newArrListSize = categoryArrList.size();
+		
+		if (newArrListSize == (oldArrListSize + 1)) {
+			added = true;
+		}
+		
+		return added;
 	}
 
 }
