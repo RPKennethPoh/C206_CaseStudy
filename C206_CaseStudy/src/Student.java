10387db9a04d7ff45484734d84f0f321526076b1
Crystal Koh <20019791@myrp.edu.sg>
2021-08-02 00:53:59 +0800

    student.java

--- C206_CaseStudy/src/Student.java
+++ C206_CaseStudy/src/Student.java
@@ -1,6 +1,6 @@
 /*
- * I declare that this code was written by me.  
- * I will not copy or allow others to copy my code. 
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
  * I understand that copying code is considered as plagiarism.
  * 
  * Student Name: Crystal Koh
