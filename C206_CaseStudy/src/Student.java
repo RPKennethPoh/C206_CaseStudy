11866c4c1480dfced0023083225d96271bdd036a
Crystal Koh <20019791@myrp.edu.sg>
2021-08-02 00:43:25 +0800

    Student.java

---
+++ C206_CaseStudy/src/Student.java
@@ -0,0 +1,76 @@
+/*
+ * I declare that this code was written by me. 
+ * I will not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: Crystal Koh
+ * Student ID: 20019791
+ * Class: C208-2B4B-E37A-FF
+ * Date/Time created: Sunday 01-08-2021 23:42
+ */
+
+/**
+ *
+ */
+public class Student {
+	private int studentId, studentWeight, studentHeight;
+	private String studentName, studentClass, studentGrade;
+	
+	public Student(int studentId, int studentWeight, int studentHeight, String studentName, String studentClass, String studentGrade) {
+		this.studentId = studentId;
+		this.studentWeight = studentWeight;
+		this.studentHeight = studentHeight;
+		this.studentName = studentName;
+		this.studentClass = studentClass;
+		this.studentGrade = studentGrade;
+	}
+
+	public int getStudentWeight() {
+		return studentWeight;
+	}
+
+	public void setStudentWeight(int studentWeight) {
+		this.studentWeight = studentWeight;
+	}
+
+	public int getStudentHeight() {
+		return studentHeight;
+	}
+
+	public void setStudentHeight(int studentHeight) {
+		this.studentHeight = studentHeight;
+	}
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getStudentClass() {
+		return studentClass;
+	}
+
+	public void setStudentClass(String studentClass) {
+		this.studentClass = studentClass;
+	}
+
+	public String getStudentGrade() {
+		return studentGrade;
+	}
+
+	public void setStudentGrade(String studentGrade) {
+		this.studentGrade = studentGrade;
+	}
+}
